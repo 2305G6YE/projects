33d64bf66fff090abc2f00082559533e09399afe
2305G6YE <ulmi910@gmail.com>
2025-05-30 11:40:18 +0900

    ADD: Filter function

--- domCrashCourseProject/main.js
+++ domCrashCourseProject/main.js
@@ -1,10 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+//Filter event
+filter.addEventListener('keyup', filterItems)
 
 //Add item
 function addItem(e) {
@@ -44,4 +47,22 @@ function removeItem(e){
         }
         
     }
+}
+
+//Filter Items
+function filterItems(e) {
+    //convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    //get lis
+    var items = itemList.getElementsByTagName('li');
+    //Convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        console.log(itemName);
+        if(itemName.toLowerCase().indexOf(text) != -1){
+           item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
 }
