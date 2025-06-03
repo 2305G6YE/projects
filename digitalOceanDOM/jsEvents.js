05097ae83d083e4a41ed992a54ca02d6afb6a5fe
2305G6YE <ulmi910@gmail.com>
2025-06-03 09:21:55 +0900

    ADD: boilerplate for event-test-p html, finish event html

--- digitalOceanDOM/jsEvents.js
+++ digitalOceanDOM/jsEvents.js
@@ -1,3 +1,4 @@
+//Button 1
 const changeText = () => {
     const p = document.querySelector('p'); 
     p.textContent = "I changed because of an inline event handler."
@@ -7,7 +8,7 @@ const changeSecondText = () => {
     const p = document.body.children[3];
     p.textContent = "I also changed, but because of an event handler property!"
 }
-
+//Button 2
 const btn = document.querySelector('#btn2');
 btn2.onclick = changeSecondText;
 
@@ -16,3 +17,23 @@ const alertText = () => {
 }
 
 btn2.onclick = alertText;
+
+//Button 3
+const changeThirdText = () => {
+    const p = document.body.children[5];
+    p.textContent = "I also changed, but because of a event listener!"
+}
+
+    // Listeners allow multiple callbacks on the same element,
+    //  but handlers only allow one, as per btn 2 example 
+const btn3 = document.querySelector('#btn3')
+btn3.addEventListener('click', changeThirdText);
+btn3.addEventListener('click', alertText);
+    //Can also do anonymous functions in listeners
+
+
+ //Test the key and code properties
+ document.addEventListener('keydown', event => {
+    console.log('key: ' + event.key);
+    console.log('code: ' + event.code);
+ })   
