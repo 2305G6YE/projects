9ff6315bd0b439371e07839555d8770cb1de945b
2305G6YE <ulmi910@gmail.com>
2025-06-13 11:22:10 +0900

    ERROR: Figure out hover effect div not being detect

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,15 +1,30 @@
- const container = document.querySelector(".container");
+ 
+const container = document.querySelector(".container");
 //Creates rows
 function makeGrid(rowNum,colNum) {
     for (let i = 0; i < rowNum; i++) {  
-        const row = document.createElement("div");
+        var row = document.createElement("div");
         row.className = "rowClass";
         container.appendChild(row);
         for (let c = 0; c < colNum; c++) {
-            const col = document.createElement("div");
+            var col = document.createElement("div");
             col.className = "colClass";
             row.appendChild(col);
         }
     }
 }
+
 makeGrid(16, 16);
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+
+//Hover Effect
+const div = document.querySelectorAll("div.rowClass, div.colClass");
+function randomColor() {
+    Math.floor(Math.random() * 255);
+};
+
+document.addEventListener("mousemove", function(e) {
+    div.style.backgroundColor = "rgb(" + `${randomColor()}` +
+    ", " + `${randomColor()}` + ", " + `${randomColor()}` + ")";
+})
